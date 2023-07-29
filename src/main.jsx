7b3aff50305554241c45f9a34efdccdc2f6e8fa1
Kamielo <mickael.bsa@gmail.com>
2023-07-29 18:52:09 +0200

    F02: Add ErrorPage and change eslint rules

--- src/main.jsx
+++ src/main.jsx
@@ -4,16 +4,18 @@ import App from './App.jsx';
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import './index.css';
 import OrderPage from './pages/OrderPage/OrderPage.jsx';
+import ErrorPage from './pages/ErrorPage/ErrorPage.jsx';
 
 const router = createBrowserRouter([
     {
         path: '/',
         element: <App />,
+        errorElement: <ErrorPage />,
     },
     {
-      path: '/order/:username',
-      element: <OrderPage />
-    }
+        path: '/order/:username',
+        element: <OrderPage />,
+    },
 ]);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
---
+++ src/pages/ErrorPage/ErrorPage.jsx
@@ -0,0 +1,15 @@
+import { useNavigate } from 'react-router-dom';
+
+export default function ErrorPage() {
+    const navigate = useNavigate();
+
+    return (
+        <div>
+            <h1>Oops !</h1>
+            <p>Sorry, an unexpected error has occurred.</p>
+            <button onClick={() => navigate('/')}>
+                Retourner vers la page d'accueil
+            </button>
+        </div>
+    );
+}
