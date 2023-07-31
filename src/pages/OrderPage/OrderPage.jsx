121660ddcb36aad7daa94ac46f88a8e85af609fa
Mickael Bossa <mickael.bossa@outlook.com>
2023-07-31 12:24:07 +0200

    F04: Add fonts and styling

--- src/pages/OrderPage/OrderPage.jsx
+++ src/pages/OrderPage/OrderPage.jsx
@@ -1,18 +1,23 @@
-import { useParams, useNavigate } from 'react-router-dom';
+import { useParams } from 'react-router-dom';
 import Logo from '../../components/ui/Logo/Logo'
+import { Link } from 'react-router-dom';
+import { BsPersonCircle } from 'react-icons/bs';
+
 import styles from './OrderPage.module.css';
 
 export default function OrderPage() {
     const params = useParams();
-    const navigate = useNavigate();
 
     return (
         <div className={styles.container}>
             <nav className={styles.navBar}>
                 <Logo />
-                <div>
-                    <h1>Hey, {params.username}</h1>
-                    <button onClick={() => navigate('/')}>Déconnexion</button>
+                <div className={styles.rightSideContainer}>
+                    <div className={styles.heyUserAndBtn}>
+                        <h1>Hey, {params.username}</h1>
+                        <Link to={'/'} className={styles.logOutBtn}>Déconnexion</Link>
+                    </div>
+                    <BsPersonCircle className={styles.personIcon} />
                 </div>
             </nav>
             <main className={styles.mainContent}>
