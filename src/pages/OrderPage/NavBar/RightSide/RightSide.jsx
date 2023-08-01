13efe5e42e30f7bf899f9b3b9e72165337731f62
Mickael Bossa <mickael.bossa@outlook.com>
2023-08-01 12:30:15 +0200

    F04: Refacto to add HeyUser component

--- src/pages/OrderPage/NavBar/RightSide/RightSide.jsx
+++ src/pages/OrderPage/NavBar/RightSide/RightSide.jsx
@@ -1,19 +1,11 @@
 import { BsPersonCircle } from 'react-icons/bs';
-import { Link } from 'react-router-dom';
 import styles from './RightSide.module.css';
+import HeyUserAndLogOut from './HeyUserAndLogOut/HeyUserAndLogOut';
 
 export default function RightSide({ params }) {
     return (
         <div className={styles.rightSideContainer}>
-            <div className={styles.heyUserAndBtn}>
-                <p>
-                    Hey,{' '}
-                    <spans className={styles.username}>{params.username}</spans>
-                </p>
-                <Link to={'/'} className={styles.logOutBtn}>
-                    Déconnexion
-                </Link>
-            </div>
+            <HeyUserAndLogOut params={params} />
             <BsPersonCircle className={styles.personIcon} />
         </div>
     );
