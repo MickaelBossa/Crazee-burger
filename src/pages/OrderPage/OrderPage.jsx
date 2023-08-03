dceba553df70590dba862c5925beb1be9a2578fe
Mickael Bossa <mickael.bossa@outlook.com>
2023-08-03 15:36:09 +0200

    F05: Add all product card content

--- src/pages/OrderPage/OrderPage.jsx
+++ src/pages/OrderPage/OrderPage.jsx
@@ -2,6 +2,7 @@ import { useParams } from 'react-router-dom';
 import { fakeMenu2 } from '../../../fakeData/fakeMenu';
 import styles from './OrderPage.module.css';
 import NavBar from './NavBar/NavBar';
+import PrimaryButton from '../../components/ui/PrimaryButton/PrimaryButton';
 
 export default function OrderPage() {
     const params = useParams();
@@ -12,7 +13,12 @@ export default function OrderPage() {
             <main className={styles.mainContent}>
                 {fakeMenu2.map((product) => (
                     <div key={product.id} className={styles.productCard}>
-                    
+                        <img src={product.imageSource} alt={`Image de ${product.title}`} />
+                        <p>{product.title}</p>
+                        <div>
+                            <p>{product.price} €</p>
+                            <PrimaryButton width={'95px'} label={'Ajouter'} height={'38px'} />
+                        </div>
                     </div>
                 ))}
             </main>
