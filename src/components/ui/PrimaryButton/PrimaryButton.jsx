9e73bc88a68f9a7281f51ca42f459ec1851f1f6a
Mickael Bossa <mickael.bossa@outlook.com>
2023-08-03 15:29:44 +0200

    F05: PrimaryButton > Add width props to make it more re usable

--- src/components/ui/PrimaryButton/PrimaryButton.jsx
+++ src/components/ui/PrimaryButton/PrimaryButton.jsx
@@ -1,8 +1,13 @@
 import styles from './PrimaryButton.module.css';
 
-export default function PrimaryButton({ Icon }) {
+export default function PrimaryButton({ Icon, width }) {
+
+    const primaryButtonStyle = {
+        width: width || '400px'
+    }
+
     return (
-        <div className={styles.container}>
+        <div className={styles.container} style={primaryButtonStyle}>
             <button type="submit">Accédez à mon espace</button>
             {Icon && Icon}
         </div>
