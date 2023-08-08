32962caa5f932eb637096ce73869c4167833544b
Mickael Bossa <mickael.bossa@outlook.com>
2023-08-08 18:46:19 +0200

    F07: Added admin panel show/hide functionality

--- src/pages/OrderPage/OrderPage.jsx
+++ src/pages/OrderPage/OrderPage.jsx
@@ -5,17 +5,27 @@ import NavBar from './NavBar/NavBar';
 import ProductCard from '../../components/ui/ProductCard/ProductCard';
 import { createContext } from 'react';
 import { useState } from 'react';
+import { FiChevronDown, FiChevronUp } from 'react-icons/fi';
+import { AiOutlinePlus } from 'react-icons/ai';
+import { MdModeEditOutline } from 'react-icons/md';
 
 export const AdminContext = createContext(null);
 
 export default function OrderPage() {
     const [isAdmin, setIsAdmin] = useState(false);
+    const [isAdminPanelVisible, setIsAdminPanelVisible] = useState(isAdmin);
 
     const params = useParams();
 
     const toggleAdminMode = () => {
-        setIsAdmin(!isAdmin)
-    }
+        setIsAdmin(!isAdmin);
+    };
+
+    const conditionalStyles = {
+        adminPanelHeight: {
+            height: isAdminPanelVisible && '0px',
+        },
+    };
 
     return (
         <AdminContext.Provider value={isAdmin}>
@@ -25,6 +35,33 @@ export default function OrderPage() {
                     {fakeMenu2.map((product) => (
                         <ProductCard key={product.id} product={product} />
                     ))}
+                    {isAdmin && (
+                        <div
+                            className={styles.adminPanel}
+                            style={conditionalStyles.adminPanelHeight}
+                        >
+                            <button
+                                className={styles.adminPanelBtn}
+                                onClick={() =>
+                                    setIsAdminPanelVisible(!isAdminPanelVisible)
+                                }
+                            >
+                                {isAdminPanelVisible ? (
+                                    <FiChevronUp />
+                                ) : (
+                                    <FiChevronDown />
+                                )}
+                            </button>
+                            <button className={styles.adminPanelBtn}>
+                                <AiOutlinePlus />
+                                Ajouter un produit
+                            </button>
+                            <button className={styles.adminPanelBtn}>
+                                <MdModeEditOutline />
+                                Modifier un produit
+                            </button>
+                        </div>
+                    )}
                 </main>
             </div>
         </AdminContext.Provider>
