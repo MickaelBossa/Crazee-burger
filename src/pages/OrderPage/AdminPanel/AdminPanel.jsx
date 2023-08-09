d50b325f4f25f37d336c3f014ab43150bd82d6a0
Mickael Bossa <mickael.bossa@outlook.com>
2023-08-09 12:13:50 +0200

    F07: Refacto> Add AdminPanel component

---
+++ src/pages/OrderPage/AdminPanel/AdminPanel.jsx
@@ -0,0 +1,71 @@
+import styles from './AdminPanel.module.css';
+import { FiChevronDown, FiChevronUp } from 'react-icons/fi';
+import { AiOutlinePlus } from 'react-icons/ai';
+import { MdModeEditOutline } from 'react-icons/md';
+
+export default function AdminPanel({
+    toggleActiveTab,
+    toggleAdminPanelVisibility,
+    activeTab,
+    isAdminPanelVisible,
+}) {
+    return (
+        <div
+            className={styles.adminPanel}
+            style={{
+                height: isAdminPanelVisible ? '0px' : '250px',
+            }}
+        >
+            <button
+                className={styles.adminPanelBtn}
+                style={{
+                    width: '62px',
+                    backgroundColor: isAdminPanelVisible && '#292729',
+                    borderTop : isAdminPanelVisible && 'none',
+                    borderBottom : isAdminPanelVisible && 'none',
+                }}
+                onClick={toggleAdminPanelVisibility}
+            >
+                {!isAdminPanelVisible ? (
+                    <FiChevronDown width={16} />
+                ) : (
+                    <FiChevronUp width={16} />
+                )}
+            </button>
+
+            <button
+                className={styles.adminPanelBtn}
+                style={{
+                    backgroundColor: activeTab === 0 && '#292729',
+                    color: activeTab === 0 && '#FFFFFF',
+                    border : activeTab === 0 && 'none',
+                }}
+                onClick={() => toggleActiveTab(0)}
+            >
+                <AiOutlinePlus width={16} />
+                Ajouter un produit
+            </button>
+
+            <button
+                className={styles.adminPanelBtn}
+                style={{
+                    backgroundColor: activeTab === 1 && '#292729',
+                    color: activeTab === 1 && '#FFFFFF',
+                    border: activeTab === 1 && 'none',
+                }}
+                onClick={() => toggleActiveTab(1)}
+            >
+                <MdModeEditOutline width={16} />
+                Modifier un produit
+            </button>
+            <p
+                className={styles.adminPanelTxt}
+                style={{
+                    display: isAdminPanelVisible && 'none',
+                }}
+            >
+                {activeTab === 0 ? 'Ajouter un produit' : 'Modifier un produit'}
+            </p>
+        </div>
+    );
+}
--- src/pages/OrderPage/OrderPage.jsx
+++ src/pages/OrderPage/OrderPage.jsx
@@ -5,14 +5,13 @@ import NavBar from './NavBar/NavBar';
 import ProductCard from '../../components/ui/ProductCard/ProductCard';
 import { createContext } from 'react';
 import { useState } from 'react';
-import { FiChevronDown, FiChevronUp } from 'react-icons/fi';
-import { AiOutlinePlus } from 'react-icons/ai';
-import { MdModeEditOutline } from 'react-icons/md';
+import AdminPanel from './AdminPanel/AdminPanel';
 
 export const AdminContext = createContext(null);
 
 export default function OrderPage() {
     const [isAdmin, setIsAdmin] = useState(false);
+
     const [isAdminPanelVisible, setIsAdminPanelVisible] = useState(isAdmin);
     const [activeTab, setActiveTab] = useState(0);
 
@@ -20,6 +19,19 @@ export default function OrderPage() {
 
     const toggleAdminMode = () => {
         setIsAdmin(!isAdmin);
+    };
+
+    const toggleActiveTab = (nbr) => {
+        if (isAdminPanelVisible) {
+            setIsAdminPanelVisible(!isAdminPanelVisible);
+            setActiveTab(nbr);
+        } else {
+            setActiveTab(nbr);
+        }
+    };
+
+    const toggleAdminPanelVisibility = () => {
+        setIsAdminPanelVisible(!isAdminPanelVisible);
     };
 
     return (
@@ -31,68 +43,14 @@ export default function OrderPage() {
                         <ProductCard key={product.id} product={product} />
                     ))}
                     {isAdmin && (
-                        <div
-                            className={styles.adminPanel}
-                            style={{
-                                height: isAdminPanelVisible ? '0px' : '250px',
-                            }}
-                        >
-                            <button
-                                className={styles.adminPanelBtn}
-                                style={{
-                                    width: '62px',
-                                    backgroundColor:
-                                        isAdminPanelVisible && '#292729',
-                                }}
-                                onClick={() =>
-                                    setIsAdminPanelVisible(!isAdminPanelVisible)
-                                }
-                            >
-                                {isAdminPanelVisible ? (
-                                    <FiChevronUp width={16} />
-                                ) : (
-                                    <FiChevronDown width={16} />
-                                )}
-                            </button>
-
-                            <button
-                                className={styles.adminPanelBtn}
-                                style={{
-                                    backgroundColor:
-                                        activeTab === 0 && '#292729',
-                                    color: activeTab === 0 && '#FFFFFF',
-                                    border: activeTab === 0 && 'none',
-                                }}
-                                onClick={() => setActiveTab(0)}
-                            >
-                                <AiOutlinePlus width={16} />
-                                Ajouter un produit
-                            </button>
-
-                            <button
-                                className={styles.adminPanelBtn}
-                                style={{
-                                    backgroundColor:
-                                        activeTab === 1 && '#292729',
-                                    color: activeTab === 1 && '#FFFFFF',
-                                    border: activeTab === 1 && 'none',
-                                }}
-                                onClick={() => setActiveTab(1)}
-                            >
-                                <MdModeEditOutline width={16} />
-                                Modifier un produit
-                            </button>
-                            <p
-                                className={styles.adminPanelTxt}
-                                style={{
-                                    display: isAdminPanelVisible && 'none',
-                                }}
-                            >
-                                {activeTab === 0
-                                    ? 'Ajouter un produit'
-                                    : 'Modifier un produit'}
-                            </p>
-                        </div>
+                        <AdminPanel
+                            toggleActiveTab={toggleActiveTab}
+                            toggleAdminPanelVisibility={
+                                toggleAdminPanelVisibility
+                            }
+                            activeTab={activeTab}
+                            isAdminPanelVisible={isAdminPanelVisible}
+                        />
                     )}
                 </main>
             </div>
