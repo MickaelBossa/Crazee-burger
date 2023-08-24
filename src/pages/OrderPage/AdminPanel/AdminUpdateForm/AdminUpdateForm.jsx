344ccadc86984b1e99355d055fa5b6bb560054fc
Mickael Bossa <mickael.bossa@outlook.com>
2023-08-24 11:32:27 +0200

    F09: Change cursor icon

--- src/pages/OrderPage/AdminPanel/AdminUpdateForm/AdminUpdateForm.jsx
+++ src/pages/OrderPage/AdminPanel/AdminUpdateForm/AdminUpdateForm.jsx
@@ -1,11 +1,13 @@
-import { LuMousePointerClick } from 'react-icons/lu';
+import { HiCursorClick } from 'react-icons/hi';
 import styles from './AdminUpdateForm.module.css';
 
 export default function AdminUpdateForm() {
     return (
         <div className={styles.updateContainer}>
-            <p className={styles.updateTxt}>Cliquez sur un produit pour le modifier</p>
-            <LuMousePointerClick className={styles.updateIcon} />
+            <p className={styles.updateTxt}>
+                Cliquez sur un produit pour le modifier
+            </p>
+            <HiCursorClick className={styles.updateIcon} />
         </div>
     );
 }
